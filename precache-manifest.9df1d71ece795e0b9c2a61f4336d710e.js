01b23b6cb062a587e9b48b195ac4d329266c63fe
Elijah Bassett <Bassett@@unc.edu>
2022-01-23 13:13:25 -0500

    Updates

---
+++ precache-manifest.9df1d71ece795e0b9c2a61f4336d710e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad4ea54bce9b6484a95470b3de449c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "665e1a687429a5815f8d",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "28bc1deb524339c76051",
+    "url": "/static/js/2.11b5fdca.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.11b5fdca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665e1a687429a5815f8d",
+    "url": "/static/js/main.128c0749.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "956b3143571199275dfe69150a7ab6e1",
+    "url": "/static/media/IMG_1224.956b3143.jpg"
+  },
+  {
+    "revision": "a699709cb82b7810c4367389029a8434",
+    "url": "/static/media/resume.a699709c.pdf"
+  }
+]);
